5679ec7e133144516214acc1219dfbac46265d01
Roland <rockilam@hotmail.com>
2020-08-21 00:33:37 -0300

    Update
    Atualizando bloqueio e acrescentando estilo de css

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
+import subprocess
 import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        if request.form['disable']:
-            os.system("ansible-playbook -i playbooks/win unblock-internet.yml")
-        elif request.form['enable']:
-            os.system("ansible-playbook -i playbooks/win block-internet.yml")
-        return render_template('index.html')
-    else:
-        return render_template('index.html')
+    status = {'status':'disconnected'}
+    return render_template('index.html', status=status)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/connect", methods=["GET", "POST"])
+def connect():
+
+    if request.form.get("status") == "CONECTAR":
+        cmd = ["ansible-playbook",
+        "-i",
+        "/home/rockson/Documentos/developer/blocknet/playbooks/win",
+        "/home/rockson/Documentos/developer/blocknet/playbooks/unblock-internet.yml"]
+
+        res = subprocess.check_output(cmd)
+        for line in res.splitlines():
+            print(line)
+        info = {"status" : "success"}
+    else:
+        cmd = ["ansible-playbook",
+        "-i",
+        "/home/rockson/Documentos/developer/blocknet/playbooks/win",
+        "/home/rockson/Documentos/developer/blocknet/playbooks/block-internet.yml"]
+
+        res = subprocess.check_output(cmd)
+        for line in res.splitlines():
+            print(line)
+        info = {"status" : "success"}
+    return jsonify(info)
